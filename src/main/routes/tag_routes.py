b60c1e8320e9a40983b384fcdd939420c0b1de95
megaVE <viniherofactory@gmail.com>
2024-02-07 20:59:35 -0300

    Second Day: Showing errors and including tests

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -2,13 +2,23 @@ from flask import Blueprint, request, jsonify
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.tag_creator_view import TagCreatorView
 
+from src.errors.error_handler import handle_errors
+
+from src.validators.tag_creator_validator import tag_creator_validator
+
 tag_routes_bp = Blueprint("tags_routes", __name__)
 
 @tag_routes_bp.route('/create_tag', methods=['POST'])
 def create_tags():
-    tag_creator_view = TagCreatorView()
+    response = None
 
-    http_request = HttpRequest(body=request.json)
-    response = tag_creator_view.validate_and_create(http_request)
+    try:
+        tag_creator_validator(request)
+        tag_creator_view = TagCreatorView()
+
+        http_request = HttpRequest(body=request.json)
+        response = tag_creator_view.validate_and_create(http_request)
+    except Exception as exception:
+        response = handle_errors(exception)
 
     return jsonify(response.body), response.status_code
